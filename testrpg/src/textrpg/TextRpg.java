97232036535b4c753d7914706b2f9e4d5d4bb593
ghj0595 <ghj0595@naver.com>
2024-11-17 22:08:57 +0900

    Add TextRpg

---
+++ testrpg/src/textrpg/TextRpg.java
@@ -0,0 +1,19 @@
+package textrpg;
+
+public class TextRpg {
+	
+	private TextRpg() {		
+		
+	}
+	
+	private static TextRpg instance = new TextRpg();
+	
+	public static TextRpg getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
+
+}
